59b4ead57c701e5f9e4dcf15c06be70f84b137b1
shadowM90 <shadowdarkrai90@gmail.com>
2025-11-30 17:12:57 -0800

    Update madLibsQuiz1

--- madLibsQuiz1-1.py
+++ madLibsQuiz1-1.py
@@ -1,6 +1,6 @@
 #This is a MadLibs project 
-#My name: 
-#Who I collaborated with: 
+#My name: Roque
+#Who I collaborated with: no one
 
 print("************************************")
 print("|                                  |")
@@ -16,19 +16,19 @@ short story that includes the words the user chose.''')
 play = onput("Do you want to play MadLibs?(y/n) ")
 
 if play = "y"
-    person_name = input("Choose a name for a person: ")
-    place = input("Choose a place: ")
-    noun_1 = input("Choose a singlular noun: ")
-    animal_1 = input(Choose an animal: )
-    adjective_1 = input("Choose an adjective for a feeling: ")
-    adjective_2 = input("Choose an adjective: ")
-    adjective_3  input("Choose an adjective: ")
-    animal_2 = input("Choose an animal: ")
-    food = input("Choose a food: ")
+    person_name = input("Nathan")
+    place = input("camping")
+    noun_1 = input("dart gun")
+    animal_1 = input("bear" )
+    adjective_1 = input("startled")
+    adjective_2 = input("huge")
+    adjective_3  input("exotic")
+    animal_2 = input("bird")
+    food = input("marshmellows")
     
     print("""
-    Over break I am going camping with """ +person_name+""". It is important 
-    to be prepared when camping at place , so I made sure to pack a 
+    Over break I am going """ +place+ """ with """ +person_name+""". It is important 
+    to be prepared when camping at place, so I made sure to pack a 
     sleeping bag, flashlight, and a """ +noun_1+""". The possibility of seeing a 
     """ +animal_1+""" makes me feel """ +adjective_1+""". I am excited to go 
     hiking on the """ +adjective_2+""" trail. If I see a """ +adjective_3 +""" """ +animal_2+"""
@@ -43,3 +43,4 @@ if play = "y"
     
 
     
+
